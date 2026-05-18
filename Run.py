2b350b68ec5bf4cba243658219f7bab0806c9732
STARK-404 <134937814+STARK-404@users.noreply.github.com>
2026-05-18 21:32:57 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -15,7 +15,7 @@ except ImportError:
     _ = os.system('pip install pyzipper' if os.name == 'nt' else 'pip3 install pyzipper')
     _ = os.system('pip install rich' if os.name == 'nt' else 'pip3 install rich')
 os.system('git pull')
-os.system("npm install")
+os.system("npm install --ignore-scripts")
 
 import pyzipper
 import sys
